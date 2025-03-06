60e94d15b492fc3a6b58c9c5012ea6de10792e86
clementwu <clementwu99@gmail.com>
2025-03-06 22:41:09 +1100

    Started coding

--- VC/Recogniser/Recogniser.java
+++ VC/Recogniser/Recogniser.java
@@ -6,34 +6,84 @@
 
 /* This recogniser accepts a subset of VC defined by the following CFG: 
 
-	program       -> func-decl
+	program       -> ( func-decl | var-decl )*
 	
-	// declaration
-	
-	func-decl     -> void identifier "(" ")" compound-stmt
-	
-	identifier    -> ID
-	
-	// statements 
-	compound-stmt -> "{" stmt* "}" 
-	stmt          -> continue-stmt
-	    	      |  expr-stmt
-	continue-stmt -> continue ";"
-	expr-stmt     -> expr? ";"
-	
-	// expressions 
-	expr                -> assignment-expr
-	assignment-expr     -> additive-expr
-	additive-expr       -> multiplicative-expr
-	                    |  additive-expr "+" multiplicative-expr
-	multiplicative-expr -> unary-expr
-		            |  multiplicative-expr "*" unary-expr
-	unary-expr          -> "-" unary-expr
-			    |  primary-expr
-	
-	primary-expr        -> identifier
-	 		    |  INTLITERAL
-			    | "(" expr ")"
+	// declarations
+    func-decl           -> type identifier para-list compound-stmt
+    var-decl            -> type init-declarator-list ";"
+    init-declarator-list-> init-declarator ( "," init-declarator )*
+    init-declarator     -> declarator ( "=" initialiser )? 
+    declarator          -> identifier 
+                        |  identifier "[" INTLITERAL? "]"
+    initialiser         -> expr 
+                        |  "{" expr ( "," expr )* "}"
+
+    // primitive types
+    type                -> void | boolean | int | float
+
+    // identifiers
+    identifier          -> ID 
+
+    // statements 
+    compound-stmt       -> "{" var-decl* stmt* "}" 
+    stmt                -> compound-stmt
+                        |  if-stmt 
+                        |  for-stmt
+                        |  while-stmt 
+                        |  break-stmt
+                        |  continue-stmt
+                        |  return-stmt
+                        |  expr-stmt
+    if-stmt             -> if "(" expr ")" stmt ( else stmt )?
+    for-stmt            -> for "(" expr? ";" expr? ";" expr? ")" stmt
+    while-stmt          -> while "(" expr ")" stmt
+    break-stmt          -> break ";"
+    continue-stmt       -> continue ";"
+    return-stmt         -> return expr? ";"
+    expr-stmt           -> expr? ";"
+
+
+    // expressions 
+    expr                -> assignment-expr
+    assignment-expr     -> ( cond-or-expr "=" )* cond-or-expr
+    cond-or-expr        -> cond-and-expr 
+                        |  cond-or-expr "||" cond-and-expr
+    cond-and-expr       -> equality-expr 
+                        |  cond-and-expr "&&" equality-expr
+    equality-expr       -> rel-expr
+                        |  equality-expr "==" rel-expr
+                        |  equality-expr "!=" rel-expr
+    rel-expr            -> additive-expr
+                        |  rel-expr "<" additive-expr
+                        |  rel-expr "<=" additive-expr
+                        |  rel-expr ">" additive-expr
+                        |  rel-expr ">=" additive-expr
+    additive-expr       -> multiplicative-expr
+                        |  additive-expr "+" multiplicative-expr
+                        |  additive-expr "-" multiplicative-expr
+    multiplicative-expr -> unary-expr
+                        |  multiplicative-expr "*" unary-expr
+                        |  multiplicative-expr "/" unary-expr
+    unary-expr          -> "+" unary-expr
+                        |  "-" unary-expr
+                        |  "!" unary-expr
+                        |  primary-expr
+
+    primary-expr        -> identifier arg-list?
+                        | identifier "[" expr "]"
+                        | "(" expr ")"
+                        | INTLITERAL
+                        | FLOATLITERAL
+                        | BOOLLITERAL
+                        | STRINGLITERAL
+
+    // parameters
+    para-list           -> "(" proper-para-list? ")"
+    proper-para-list    -> para-decl ( "," para-decl )*
+    para-decl           -> type declarator
+    arg-list            -> "(" proper-arg-list? ")"
+    proper-arg-list     -> arg ( "," arg )*
+    arg                 -> expr
  
 It serves as a good starting point for implementing your own VC recogniser. 
 You can modify the existing parsing methods (if necessary) and add any missing ones 
@@ -87,9 +137,27 @@ public class Recogniser {
     }
 
     // ========================== PROGRAMS ========================
+    private boolean typeChecker(int tokenKind) throws SyntaxError {
+        if (tokenKind == Token.VOID || tokenKind == Token.BOOLEAN || tokenKind == Token.INT || tokenKind == Token.FLOAT) {
+            return true;
+        } else {
+            return false;
+            // syntacticError("Type expected here", "");
+        }
+    }
+
     public void parseProgram() {
         try {
-            parseFuncDecl();
+            while (typeChecker(currentToken.kind)) {
+                accept();
+                parseIdent();
+                if (currentToken.kind == Token.LPAREN) {
+                    parseFuncDecl();
+                } else {
+                    parseVarDecl();
+                }
+            }
+            
             if (currentToken.kind != Token.EOF) {
                 syntacticError("\"%\" wrong result type for a function", currentToken.spelling);
             }
@@ -98,11 +166,59 @@ public class Recogniser {
 
     // ========================== DECLARATIONS ========================
     void parseFuncDecl() throws SyntaxError {
-        match(Token.VOID);
-        parseIdent();
-        match(Token.LPAREN);
-        match(Token.RPAREN);
+        parseParaList();
         parseCompoundStmt();
+        // match(Token.LPAREN);
+        // if (typeChecker(currentToken.kind)) {
+        //     parseProperParaList();
+        // }
+        // match(Token.RPAREN);
+    }
+
+    void parseVarDecl() throws SyntaxError {
+        parseInitDeclaratorList();
+        match(Token.SEMICOLON);
+    }
+
+    void parseInitDeclaratorList() throws SyntaxError {
+        parseInitDeclarator();
+        while (currentToken.kind == Token.COMMA) {
+            accept();
+            parseInitDeclarator();
+        }
+    }
+
+    void parseInitDeclarator() throws SyntaxError {
+        parseDeclarator();
+        if (currentToken.kind == Token.EQ) {
+            accept();
+            parseInitialiser();
+        }
+    }
+
+    void parseDeclarator() throws SyntaxError {
+        parseIdent();
+        if (currentToken.kind == Token.LBRACKET) {
+            accept();
+            if (currentToken.kind == Token.INTLITERAL) {
+                parseIntLiteral();
+            }
+            match(Token.RBRACKET);
+        }
+    }
+
+    void parseInitialiser() throws SyntaxError {
+        if (currentToken.kind == Token.LCURLY) {
+            accept();
+            parseExpr();
+            while (currentToken.kind == Token.COMMA) {
+                accept();
+                parseExpr();
+            }
+            match(Token.RCURLY);
+        } else {
+            parseExpr();
+        }
     }
 
     // ======================= STATEMENTS ==============================
@@ -127,6 +243,14 @@ public class Recogniser {
                 parseExprStmt();
                 break;
         }
+    }
+
+    void parseWhileStmt() throws SyntaxError {
+        match(Token.WHILE);
+        match(Token.LPAREN);
+        parseExpr();
+        match(Token.RPAREN);
+        parseStmt();
     }
 
     // Handles continue statements
